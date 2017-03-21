35936ccac6b63d35a7b25284be6290e4c0721179
André Krebs <andre.krebssilva@gmail.com>
2017-03-21 20:31:15 -0300

    Geral

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -5,7 +5,8 @@ Route::get('/', function(){
 });
 
 Route::get('/produtos', 'ProdutoController@lista');
-
+Route::get('/produtos/novo', 'ProdutoController@novo');
+Route::post('/produtos/adiciona', 'ProdutoController@adiciona');
 // ao adicionar o where na rota evitamos o problema de ambiquidade nas rotas,
 // exemplo: produtos/mostra/adiciona, dependendo da ordem pode dar erro se não usar o where
 Route::get('/produtos/mostra/{id}', 'ProdutoController@mostra')->where('id', '[0-9]+');
