cdb2d0a396c24a3526e70a833889c17de85c97cb
André Krebs <andre.krebssilva@gmail.com>
2017-04-10 21:08:44 -0300

    Comando para add model Produto e model Produto

---
+++ app/Produto.php
@@ -0,0 +1,14 @@
+<?php namespace estoque;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Produto extends Model {
+
+	//
+
+}
+
+/*
+Comando para criar a model:
+php artisan make:model Produto
+*/
