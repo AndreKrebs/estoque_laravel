d1d080ec7c1130da0d74954f0ee0b69f93b1cd91
André Krebs <andre.krebssilva@gmail.com>
2017-07-05 21:34:41 -0300

    Arquivo de seed para categorias

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,19 +2,30 @@
 
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
+use estoque\Categoria;
 
 class DatabaseSeeder extends Seeder {
 
-	/**
-	 * Run the database seeds.
-	 *
-	 * @return void
-	 */
 	public function run()
 	{
 		Model::unguard();
 
-		// $this->call('UserTableSeeder');
+		$this->call('CategoriaTableSeeder');
+	}
+
+}
+
+
+// a classe pode ser separada para ficar mais organizado
+class CategoriaTableSeeder extends Seeder {
+
+	// para rodar: "php artisan db:seed" ou "php artisan db:seed --class=CategoriaTableSeeder" para 
+	// rodar uma classe especifica de seed
+	public function run() {
+		Categoria::create(['nome' => 'Eletronico']);
+		Categoria::create(['nome' => 'Eletroeletronico']);
+		Categoria::create(['nome' => 'Brinquedo']);
+		Categoria::create(['nome' => 'Esportes']);	
 	}
 
 }
